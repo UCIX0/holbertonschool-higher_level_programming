1350918af393923a61e474d6a6fba11d5892d0a7
UCIX0 <javier.uc@outlook.com>
2023-05-30 12:28:03 -0600

    import adv 100r

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,23 +2,15 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operator = sys.argv[2]
-
-        if operator == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif operator == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    result = operators[sys.argv[2]](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
