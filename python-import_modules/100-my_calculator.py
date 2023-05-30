9a53210df1132e8b4c07f0df0bfe58271834af25
UCIX0 <javier.uc@outlook.com>
2023-05-30 12:32:08 -0600

    import adv 100rr

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
     operators = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in operators:
+    if argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    result = operators[sys.argv[2]](a, b)
-    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
+        exit(1)
+    result = operators[argv[2]](a, b)
+    print("{} {} {} = {}".format(a, argv[2], b, result))
