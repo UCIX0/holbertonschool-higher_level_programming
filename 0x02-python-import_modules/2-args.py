c2dcf548ab637659e9047984bb09cefa2bcde291
UCIX0 <javier.uc@outlook.com>
2023-05-30 11:40:41 -0600

    prinfformat import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument:\n1:", argv[1])
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print(f"{len(argv)-1} arguments:")
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print(f"{i}: {argv[i]}")
+            print("{}: {}".format(i, argv[i]))
