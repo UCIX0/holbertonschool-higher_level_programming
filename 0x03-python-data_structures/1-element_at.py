e930507f867fac2ef952b10f14fa15256e91662e
UCIX0 <javier.uc@outlook.com>
2023-06-07 10:59:00 -0600

    estruutras2

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    tam = len(my_list)
-    if idx < 0:
-        return None
-    elif idx > tam - 1:
-        return None
-    else:
-        return my_list[idx]
--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    string2 = ""
-    for i in my_string:
-        if i != "c" and i != "C":
-            string2 += i
-    return string2
