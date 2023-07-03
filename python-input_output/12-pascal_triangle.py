411644f60ab13fcbbb834c1193d23573086bf896
UCIX0 <javier.uc@outlook.com>
2023-07-02 22:54:34 -0600

    circle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -14,3 +14,13 @@ def pascal_triangle(n):
             row.append(1)
         pascal_list.append(row)
     return
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+print_triangle(pascal_triangle(5))
