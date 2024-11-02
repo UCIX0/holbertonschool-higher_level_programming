4ac468b9e77a66f683316501fc4ec1dd362038eb
Javier Uc Ix <javier.uc@outlook.com>
2024-11-02 15:07:25 -0600

    10 more struturas

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return (None)
+        return None
     else:
-        key = 0
-        item = 0
-        for i in a_dictionary:
-            if item < a_dictionary[i]:
-                item = a_dictionary[i]
-                key = i
-        return (key)
+        return max(a_dictionary, key=a_dictionary.get)
