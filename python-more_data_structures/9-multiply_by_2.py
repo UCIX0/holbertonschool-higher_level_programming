684db785bbef533a0479aa5470b5209c8f7ad196
Javier Uc Ix <javier.uc@outlook.com>
2024-11-02 02:11:22 -0600

    9 more struturas

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    b_dictionary = a_dictionary.copy()
-    for i in b_dictionary:
-        b_dictionary[i] = 2 * b_dictionary[i]
-    return(b_dictionary)
+    return {key: value * 2 for key, value in a_dictionary.items()}
