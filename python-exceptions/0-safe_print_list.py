5b17202d03d83684e87908b23390d868a1937460
UCIX <90123912+UCIX0@users.noreply.github.com>
2024-11-03 13:32:18 -0600

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    o = 0
     try:
-        for i in range(x):
+        for i in range(0, x):
             print(my_list[i], end="")
+            o += 1
     except IndexError:
         pass
     print()
-    return min(len(my_list), x)
+    return o
